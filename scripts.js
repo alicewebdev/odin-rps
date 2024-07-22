8176c885faf2a6737ca96027fce2a476b036f191
alicewebdev <174149938+alicewebdev@users.noreply.github.com>
2024-07-22 13:57:30 +0200

    Function playRound, humanWins, changes to humanResult to capitalize first letter

--- scripts.js
+++ scripts.js
@@ -6,9 +6,9 @@ function getComputerChoice(){
 
 function getHumanChoice(){
     const humanChoice = String(window.prompt("Rock, paper or scissors?", ""));
-    let humanResult = humanChoice.toLowerCase();
+    let humanResult = humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1).toLowerCase();
     
-    if (humanResult === "rock" || humanResult === "paper" || humanResult === "scissors")
+    if (humanResult === "Rock" || humanResult === "Paper" || humanResult === "Scissors")
         {return humanResult;} 
     else
         {alert("Please enter rock, paper or scissors.");
@@ -17,3 +17,30 @@ function getHumanChoice(){
 
 let humanScore = 0;
 let computerScore = 0;
+
+function humanWins(human, computer) {
+    return (
+      (human === "Rock" && computer === "Scissors") ||
+      (human === "Scissors" && computer === "Paper") ||
+      (human === "Paper" && computer === "Rock")
+    );
+  }
+
+function playRound(humanResult, computerChoice){
+    if (humanResult === computerChoice) {
+        console.log(`You tied! Both chose ${humanResult}`);
+      } 
+    else if (humanWins(humanResult, computerChoice)) {
+        humanScore++;
+        console.log(`You win! ${humanResult} beats ${computerChoice}`);
+      }
+    else {
+        computerScore++;
+        console.log(`Computer wins! ${computerChoice} beats ${humanResult}`);
+      }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
